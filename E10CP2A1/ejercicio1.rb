867fd720c06d54d2361d73d2410660450308cbd1
Alvaro Rios <alvaro@smartsites.cl>
2018-07-05 19:56:17 -0400

    Ejercicio 1

---
+++ E10CP2A1/ejercicio1.rb
@@ -0,0 +1,21 @@
+class Alumno
+  def initialize(nombre, nota1, nota2, nota3, nota4)
+    @nombre = nombre
+    @nota1 = nota1
+    @nota2 = nota2
+    @nota3 = nota3
+    @nota4 = nota4
+  end
+
+  def self.read_files(notas = 'notas.txt')
+    alumnos = []
+    data = []
+    File.open(notas, 'r') { |file| data = file.readlines }
+    data.each do |alumno|
+      alumnos << Alumno.new(*alumno.split(', '))
+    end
+    alumnos
+  end
+end
+
+print Alumno.read_files
